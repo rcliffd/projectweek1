c439982ef9b7f56781971f0fda5cbaa3864431a2
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-05-21 18:06:26 -0500

    adding stuff

--- app.js
+++ app.js
@@ -14,34 +14,26 @@ $.ajax({
     method: 'GET'
 }).then(function(response){
 
+    var video = $('<video />', {
+        id: 'video',
+        src: videoSrc,
+        type: 'video/mp4',
+        controls: true
+    });
+    video.appendTo($('#videoPlayer'))
+
     var results = response;
     console.log(results)
 
     docuemnt.write(results)
+
+
 })
 
-$("body").on("click", "button", function() {
-    console.log("clicked");
-    var buttonText = $(this).attr("id");
-    var apikey = "w6louPuZQ3IE5T5h9AcEP91gPGYNd2by";
-    var queryURL =
-      "http://api.giphy.com/v1/gifs/search?q=" +
-      buttonText +
-      "&api_key=" +
-      apikey +
-      "&limit=10";
-    //Get array of objects from giphy API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      //Add gifs to page
-      var results = response.data;
-    }
 
 
-// var oneMusicURL="https://ideas2it-onemusic.p.rapidapi.com/20131025/release?title=Doolittle&artist=Pixies&maxResultCount=5&user_key=c0f18945b5be93d362d9f8a57cb4bfd6&inc=images"
+
+var oneMusicURL="https://ideas2it-onemusic.p.rapidapi.com/20131025/release?title=Doolittle&artist=Pixies&maxResultCount=5&user_key=c0f18945b5be93d362d9f8a57cb4bfd6&inc=images"
 
 // var oneMusicTwo="https://api.onemusicapi.com/20151208/release?title=Doolittle&artist=Pixies&user_key=c0f18945b5be93d362d9f8a57cb4bfd6&inc=images&maxResultCount=1"
 
@@ -54,4 +46,4 @@ $("body").on("click", "button", function() {
 //     console.log(results)
 // }).catch(function(err) {
 //     console.log(err)
- 
+// })
