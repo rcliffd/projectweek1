b37f1f71feb3dc70fd5c6943b1da6ffd9ecd655c
Ash <Ashleigh.lund@gmail.com>
2019-05-23 19:41:04 -0500

    javascript

--- app.js
+++ app.js
@@ -1,47 +1,70 @@
-const search = "tswift";
-
-var apiKey = "AIzaSyCkrudrWOfe_tgbrb2Vhuyg9nWVkGRyvZc";
-
-// var url1 = 'http://gdata.youtube.com/feeds/api/videos?q=foo&alt=json/channels?key=' + apiKey;
-
-var url1 =
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" +
-  search +
-  "&maxResults=25&key=" +
-  apiKey;
-
-// var url1 = 'https://www.googleapis.com/youtube/v3/channels?key=' + apiKey ;
-
+const search = 'TaylorSwift'
+const apiKey = 'AIzaSyCkrudrWOfe_tgbrb2Vhuyg9nWVkGRyvZc'
+const apiKey10 = "3210dee919595df6a421526e3f0a6d13"
+const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + search + '&type=video&videoEmbeddable=true&order=viewCount' + '&maxResults=25&key=' + apiKey
+const url1 = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dJZAoTE5oJZcb8BOEXkAExAi85YngNzf&limit=10"
+//?method=artist.gettoptracks
+const url2 = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+ search +"&api_key="+ apiKey10 +"&format=json"
+//Get Artist TOP Tracks
+const url3 = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+ search +"&api_key="+ apiKey10 +"&format=json"
+// ================== YOUTUBE API ==================== // 
+AOS.init();
 $.ajax({
-  url: url1,
-  method: "GET"
-}).then(function(response) {
-  //    // var video = $('<video />', {
-  //         id: 'video',
-  //         src: videoSrc,
-  //         type: 'video/mp4',
-  //         controls: true
-  //     });
-  //     video.appendTo($('#videoPlayer'))
-
-  var results = response;
-  console.log(results);
-
-  docuemnt.write(results);
-});
-
-var oneMusicURL =
-  "https://ideas2it-onemusic.p.rapidapi.com/20131025/release?title=Doolittle&artist=Pixies&maxResultCount=5&user_key=c0f18945b5be93d362d9f8a57cb4bfd6&inc=images";
-
-// var oneMusicTwo="https://api.onemusicapi.com/20151208/release?title=Doolittle&artist=Pixies&user_key=c0f18945b5be93d362d9f8a57cb4bfd6&inc=images&maxResultCount=1"
-
-// $.ajax({
-//     url: oneMusicTwo,
-//     method: 'GET'
-// }).then(function(response){
-
-//     var results = response;
-//     console.log(results)
-// }).catch(function(err) {
-//     console.log(err)
-// })
+    url: url, 
+    method: 'GET'
+}).then(function(response){
+   // ================== GIPHY API ==================== // 
+    $.ajax({
+        url: url1,
+        method: 'GET'
+    }).then(function(gifResponse){
+        for(var j = 0; j < 9; j++){
+            var gif = $('<img>', {
+                id: 'gifImg',
+                src: gifResponse.data[j].images.fixed_height.url
+            })
+            gif.appendTo($('#gif'))
+            console.log(gifResponse)
+        }
+    })
+// ================== Last FM API ==================== // 
+    $.ajax({
+        url: url2,
+        method: 'GET'
+    }).then(function(lastFMResponse){
+        console.log(lastFMResponse)
+        var artist = lastFMResponse.artist.name
+        var artistBio = lastFMResponse.artist.bio.summary
+        console.log(artist + " " + artistBio)
+        $('#artist').append(artist)
+        $('#artistBio').append(artistBio) 
+    })
+// ================== Last FM API For Getting Top Tracks ==================== // 
+    $.ajax({
+        url: url3,
+        method: 'GET'
+    }).then(function(lastFMResponseTopTracks) {
+        console.log(lastFMResponseTopTracks)
+      
+        for(var a = 0; a < 10; a++){
+        console.log(lastFMResponseTopTracks.toptracks.track[a].name)
+        console.log(lastFMResponseTopTracks.toptracks.track[a].url)
+        var topTracks = lastFMResponseTopTracks.toptracks.track[a].name
+        $('#topTracks').append(topTracks + "<br>")
+        }
+    })
+    for (var i = 0; i < 9; i++) {
+    var videoSrc='https://www.youtube.com/embed/' + response.items[i].id.videoId + '?autoplay=0'
+    console.log(videoSrc)
+    var video = $('<iframe />', {
+        id: 'video',
+        src: videoSrc
+    });
+    video.appendTo($('#videoPlayer'))
+    var results = response;
+    console.log(results.items)
+    }
+})
+// Init Animations data-aos = ...
+AOS.init();
+Collapse
